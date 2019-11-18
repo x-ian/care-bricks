989f75fd3f47e4d9ce7ade2f7f1ea1261d874dc1
x-ian <x-ian@x-ian.net>
2019-11-18 21:23:22 +0100

    SDF

---
+++ docs/deployment-updates.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#/usr/bin/killall python
+#sleep 10
+
+#cd /home/neumann/workflow-poc-emr/bootstrap-studio-export
+#/usr/bin/python -m SimpleHTTPServer &
+
+cd ~
+killall node-red
+nohup node-red &
+  
+cd workflow-poc-emr/bootstrap-studio-export
+sed -i 's/http:\/\/localhost:8000\//assets\/js\//g' *.html
+sed -i 's/http:\/\/localhost:8000\//assets\/js\//g' assets/js/pages-event-handlers.js 
+ln -s /home/neumann/.node-red/flows_s17974030.onlinehome-server.info.json /home/neumann/workflow-poc-emr/bootstrap-studio-export/assets/jsnode-red-flows.json
