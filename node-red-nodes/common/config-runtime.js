b8457bad9daf42390f9832b6cc55c0159361c829
x-ian <x-ian@x-ian.net>
2020-04-30 20:10:22 +0200

    removed

--- node-red-nodes/common/config-runtime.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(RED) {
-    function ConfigRuntimeNode(n) {
-        RED.nodes.createNode(this,n);
-        this.flowpath = n.flowpath;
-    }
-    RED.nodes.registerType("config-runtime",ConfigRuntimeNode);
-}
