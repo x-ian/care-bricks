85c8edb4e1e93eeb0687749c98822ddde57e84fc
x-ian <x-ian@x-ian.net>
2020-11-05 14:59:30 +0100

    fhir primitive datatype

---
+++ client-js/bricks/visit-question-primitive.js
@@ -0,0 +1,32 @@
+var name = "";
+
+function statusOfNextButton() {
+	$('#navigation-next').prop('disabled', ($('#input').val().length == 0));
+}
+
+function onLoadVisitQuestionPrimitive() {
+	statusOfNextButton();
+	processPageDemographicAttribute();
+
+	let nodeid = getUrlParam('nodeid');
+	var node = nodeById(jsonFlow, nodeid);
+	$('#input-label').contents().last().replaceWith(node.label);
+	name = node.name;
+
+	loadCurrentPatient(function() {
+		$('#input').val(currentPatient.currentEncounter[name]);
+	});
+
+	$('.btn').click(function(e) {
+		defaultButtonAlphapad(e);
+		defaultButtonKeypad(e);
+		defaultButtonDatepad(e);
+		defaultButtonBooleanpad(e);
+		statusOfNextButton();
+	});
+}
+
+function hookNextVisitQuestion(e) {
+	currentPatient.currentEncounter[name] = $('#input').val();
+	updateCurrentPatient(currentPatient);
+}
