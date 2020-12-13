626b1163ba71b4a2a3b31eb1879a0fa3ce210ae9
x-ian <x-ian@x-ian.net>
2020-12-13 17:02:30 +0100

    config stuff

--- node-red-userdir/nodes/careflow-config.js
+++ node-red-userdir/nodes/careflow-config.js
@@ -2,7 +2,7 @@
 module.exports = function(RED) {
   function CareflowConfigNode(config) {
       RED.nodes.createNode(this,config);
-      this.name = config.name;
+      this.environment = config.environment;
       this.runtime_flow_path = config.runtime_flow_path;
   }
   RED.nodes.registerType("careflow-config",CareflowConfigNode);
