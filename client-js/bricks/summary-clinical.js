01f23bfdbff8a64040fa4ba56afad4c1bcdd0a76
x-ian <x-ian@x-ian.net>
2020-12-15 11:34:04 +0100

    summary-clinical added

---
+++ client-js/bricks/summary-clinical.js
@@ -0,0 +1,14 @@
+function onLoadSummaryInteractive() {
+	loadCurrentPatient(function() {
+		$('#input-label').contents().last().replaceWith("Clinical summary for " + currentPatient.givenname + " " + currentPatient.familyname);
+		
+		let nodeid = getUrlParam('nodeid');
+		let next = nextNodes(jsonFlow, nodeById(jsonFlow, nodeid));
+		let newUrl = next.type + ".html?nodeid=" + next.id;
+
+		let div = $('#all-transitions');
+
+		let node = nodeById(jsonFlow, nodeid);
+
+	});
+}
