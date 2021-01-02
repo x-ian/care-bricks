e72b4280b520bd4c1524bd589b505a85430e4f83
x-ian <x-ian@x-ian.net>
2021-01-02 15:17:03 +0100

    added

---
+++ client-js/bricks/sms-send-message.js
@@ -0,0 +1,24 @@
+function onLoadSmsSendMessage() {
+	loadCurrentPatient();
+	// loadCurrentEncounter(function() {
+	// 	$('#input-label').contents().last().replaceWith("Data summary for " + currentPatient.givenname + " " + currentPatient.familyname);
+	//
+	// 	let nodeid = getUrlParam('nodeid');
+	// 	let next = nextNodes(jsonFlow, nodeById(jsonFlow, nodeid));
+	// 	let newUrl = next.type + ".html?nodeid=" + next.id;
+	//
+	// 	let div = $('#all-transitions');
+	//
+	// 	let node = nodeById(jsonFlow, nodeid);
+	//
+	// 	if (node.info === undefined) {
+	//
+	// 		$('#summary').append('<pre>' + JSON.stringify(currentEncounter, null, 2) + '</pre>');
+	// 		$('#summary').append('<pre>' + JSON.stringify(currentPatient, null, 2) + '</pre>');
+	// 	} else {
+	// 		$('#summary').append(node.info);
+	// 		// $('#summary').append('<pre>' + node.info + '</pre>');
+	// 	}
+	//
+	// });
+}
