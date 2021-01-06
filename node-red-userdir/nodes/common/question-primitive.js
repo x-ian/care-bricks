317b25b2998ab93c3074cd8fc6c996684520d3ea
x-ian <x-ian@x-ian.net>
2021-01-07 00:09:14 +0100

    generalized

---
+++ node-red-userdir/nodes/common/question-primitive.js
@@ -0,0 +1,10 @@
+module.exports = function(RED) {
+    function QuestionPrimitiveNode(properties) {
+        RED.nodes.createNode(this,properties);
+        this.label = properties.label;
+        this.scope = properties.scope;
+        this.key = properties.key;
+        this.datatype = properties.datatype;
+    }
+    RED.nodes.registerType("question-primitive",QuestionPrimitiveNode);
+}
