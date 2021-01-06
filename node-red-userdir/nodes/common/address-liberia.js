43b94dddcd85131a8295688ab1eef94915a60613
x-ian <x-ian@x-ian.net>
2021-01-07 00:08:27 +0100

    added label

--- node-red-userdir/nodes/common/address-liberia.js
+++ node-red-userdir/nodes/common/address-liberia.js
@@ -3,6 +3,7 @@ module.exports = function(RED) {
       RED.nodes.createNode(this,config);
       var node = this;
       node.scope = config.scope;
+	  node.label = config.label;
       node.key = config.key;
   }
   RED.nodes.registerType("address-liberia",ScreenAddressLiberiaNode);
